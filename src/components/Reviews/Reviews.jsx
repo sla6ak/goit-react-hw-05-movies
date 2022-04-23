263d23cf0c8844cf691aac006f41f177d59a7a48
slabak <slabakxaker@gmail.com>
2022-04-23 04:11:06 +0300

    remove clg

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -4,7 +4,6 @@ import { useOutletContext } from 'react-router-dom'
 const Reviews = props => {
   const [filmInfo] = useOutletContext()
   const reviews = filmInfo.reviews.results
-  console.log(reviews)
 
   return (
     <>
